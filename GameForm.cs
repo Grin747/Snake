6a6c8423296b5ff6cf502412ba255a0c2dabc3b5
Grin <grinlikegreenbutgrin@gmail.com>
2019-05-04 04:04:12 +0500

    v0.1
    - Added stones
    - Started map generation

--- GameForm.cs
+++ GameForm.cs
@@ -3,43 +3,66 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace snake
 {
     class GameForm : Form
     {
-        Timer time = new Timer();
+        Timer time;
         ICell[,] map;
+        Point direction;
         
         public GameForm()
         {
             ControlBox = false;
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
             StartPosition = FormStartPosition.CenterScreen;
-            Size = new Size(962, 642);
             BackgroundImage = Image.FromFile("res//Wall.png");
             BackgroundImageLayout = ImageLayout.Tile;
+            DoubleBuffered = true;
+            direction = new Point(1, 0);
 
-            map = CreateMap(" BH ");
+            map = CreateMap(File.ReadAllLines("res//1_lvl.txt"));
+            Size = new Size(map.GetLength(1)*32, map.GetLength(0)*32);
 
+
+            time = new Timer();
             time.Interval = 1000;
             time.Tick += Tick;
             time.Start();
         }
 
-        ICell[,] CreateMap(string plan)
+        ICell[,] CreateMap(string[] lines)
         {
-            return null;
+            var res = new ICell[lines.Length, lines[0].Length];
+            for (int i = 0; i < lines.Length; i++)
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    //TODO
+                    switch (lines[i][j])
+                    {
+                        case ' ': res[i, j] = null;
+                            continue;
+                        case 'S': res[i, j] = new Stone(j*32, i*32);
+                            continue;
+                        case 'B':
+                            continue;
+                    }
+                }
+            return res;
         }
 
         void Tick(object sender, EventArgs e) => Invalidate();
 
-        void GameForm_Paint(object sender, PaintEventArgs e)
+        protected override void OnPaint (PaintEventArgs e)
         {
-            foreach (var cell in map) cell.Draw(e.Graphics);
+            foreach(var cell in map)
+                if(cell != null) cell.Draw(e.Graphics);
         }
+
+
 
         void GameForm_KeyDown(object sender, KeyEventArgs e)
         {
---
+++ Stone.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace snake
+{
+    internal class Stone : ICell
+    {
+        Image texture;
+        Rectangle rect;
+
+        public Stone(int x, int y)
+        {
+            texture = Image.FromFile("res//Stone.png");
+            rect = new Rectangle(x, y, 32, 32);
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.DrawImage(texture, rect);
+        }
+    }
+}
